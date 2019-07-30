76dcd1b700250e6ea870d9c400bcfe3c380ff49c
polizois <polizoispz@gmail.com>
2019-07-30 18:20:46 +0300

    typo fix in definitions.h

--- include/definitions.h
+++ include/definitions.h
@@ -5,7 +5,7 @@
 #define NETWORK_DEVICE "eth0" // (or "wlan0" or "usb0") The name of the network device that will be used by the program
 #define PING_PACKETS 20       // The number of packets sent to ping the broadcast ip (in order to find the available devices in the netwirk)
 #define MAX_SOCKETS 30        // The maximum allowed number of connected devices
-#define CIRC_BUFFER_SIZE 2000 // The maximum number of temporary messages the device can store before the start getting replaced by the newer once
+#define CIRC_BUFFER_SIZE 2000 // The maximum number of temporary messages the device can store before the start getting replaced by the newer ones
 #define MSG_SIZE 299          // The maximum size of a complete message "origin_destination_timestamp_actualMessage"
 #define TRUE 1                // True
 #define FALSE 0               // False
